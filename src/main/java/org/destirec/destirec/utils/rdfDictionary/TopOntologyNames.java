a02b127a1f6f55f1c09d8406e206fc0aa74263f6
havenousername <andrei.cristea997@gmail.com>
2025-05-01 22:04:41 +0200

    feat: add new class name string mapping files (reduce services injections, replace with static imports)

---
+++ src/main/java/org/destirec/destirec/utils/rdfDictionary/TopOntologyNames.java
@@ -0,0 +1,26 @@
+package org.destirec.destirec.utils.rdfDictionary;
+
+import org.destirec.destirec.rdf4j.vocabulary.DESTIREC;
+
+public final class TopOntologyNames {
+
+    private TopOntologyNames() {}
+
+    public final static class Classes {
+        public final static String TOP_ONTOLOGY  = DESTIREC.wrapNamespace("TopOntology");
+
+        public final static String OBJECT  = DESTIREC.wrapNamespace("Object");
+
+        public final static String CONCEPT  = DESTIREC.wrapNamespace("Concept");
+
+
+        public final static String ACTOR  = DESTIREC.wrapNamespace("Actor");
+
+        public final static String EVENT  = DESTIREC.wrapNamespace("Event");
+
+    }
+
+    public final static class Properties {
+        public final static String HAS_CONCEPT = DESTIREC.wrapNamespace("hasConcept");
+    }
+}
