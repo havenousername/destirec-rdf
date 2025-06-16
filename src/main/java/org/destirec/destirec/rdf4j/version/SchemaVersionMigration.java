7adb431fa76010473df63ed4f7e316b2549e632a
havenousername <andrei.cristea997@gmail.com>
2025-06-16 22:31:45 +0200

    fix: top ontology bug

--- src/main/java/org/destirec/destirec/rdf4j/version/SchemaVersionMigration.java
+++ src/main/java/org/destirec/destirec/rdf4j/version/SchemaVersionMigration.java
@@ -3,6 +3,7 @@ package org.destirec.destirec.rdf4j.version;
 import org.destirec.destirec.rdf4j.interfaces.IriMigration;
 import org.destirec.destirec.rdf4j.vocabulary.WIKIDATA;
 import org.destirec.destirec.utils.rdfDictionary.TopOntologyNames;
+import org.eclipse.rdf4j.model.vocabulary.OWL;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.eclipse.rdf4j.model.vocabulary.SKOS;
@@ -19,8 +20,10 @@ public class SchemaVersionMigration extends IriMigration {
     @Override
     protected void setupProperties() {
         builder
-                .add(get(), RDF.TYPE, TopOntologyNames.Classes.CONFIG)
+                .add(get(), RDF.TYPE, OWL.CLASS)
                 .add(get(), RDFS.LABEL,"Schema Version Class")
+                .add(get(), RDFS.SUBCLASSOF, TopOntologyNames.Classes.CONFIG.rdfIri())
+                .add(get(), RDFS.COMMENT, "Class for the schema version")
                 .add(get(), SKOS.RELATED, WIKIDATA.SOFTWARE_VERSION);
     }
 }
